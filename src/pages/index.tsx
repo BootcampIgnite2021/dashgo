12d9d649279b6a4b933848e7e742f221fcfee7f5
Francisco Junior <franciscojn49@gmail.com>
2022-04-29 11:24:12 -0300

    feat: project

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { Flex, Button, Stack} from "@chakra-ui/react"
+import { Input } from "../components/Form/Input"
+
+
+
+
+export default function SignIn() {
+  return (
+  <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+  <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius="8" flexDirection="column">
+    <Stack spacing="4">
+      <Input name="email" type="email" label="E-Mail" />
+      <Input name="password" type="password" label="Senha" />
+    </Stack>
+
+
+<Button type="submit" marginTop="6" colorScheme='pink'>
+  Entrar
+</Button>
+  </Flex>
+  </Flex>
+  )
+}
