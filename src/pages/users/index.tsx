b09d2f03fa780ffec956bcf56fe5681b17bde1a2
Francisco Júnior <franciscojn49@gmail.com>
2022-10-30 12:23:51 -0300

    feat: finish responsive aplication

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,6 +12,7 @@ import {
   Th,
   Thead,
   Tr,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine, RiEditLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
@@ -19,6 +20,11 @@ import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -46,17 +52,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
 
@@ -69,19 +75,7 @@ export default function UserList() {
                   </Box>
                 </Td>
 
-                <Td>12 de Setembro, 2022</Td>
-
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon fontSize={20} as={RiPencilLine} />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>12 de Setembro, 2022</Td>}
               </Tr>
             </Tbody>
           </Table>
